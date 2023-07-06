1538fb5182f80fce5d65c11b6c23b69ae9a82f83
ringa-nyamvula <nyamvularinga@gmail.com>
2023-07-06 13:06:44 +0300

    Complete assignment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,12 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(username: params[:username])
+    if user
     session[:user_id] = user.id
     render json: user
+    else
+      render json: {error: "Unauthorized User"}, status: :unauthorized
+    end
   end
 
   def destroy
